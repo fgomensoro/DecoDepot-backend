f3b978c1bad79555b2b5e3c9838c3b9b4a9df7e3
fgomensoro <102269369+fgomensoro@users.noreply.github.com>
2022-11-10 11:14:03 -0300

    feat: combo seeder

--- seeders/categorySeeder.js
+++ seeders/categorySeeder.js
@@ -3,13 +3,12 @@ const Category = require("../models/Category");
 const { mongoose } = require("mongoose");
 
 module.exports = async () => {
-  console.log("seeder categories entre");
   const categories = ["table", "chair", "bed", "sofa"];
   for (let i = 0; i < categories.length; i++) {
     const category = new Category({
       name: categories[i],
     });
-    // product.categories.push()
+
     await category.save();
   }
 
