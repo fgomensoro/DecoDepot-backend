2615c7da86ab15c7400289c9c77442edf7d3ada7
German-uruguay <106714484+German-uruguay@users.noreply.github.com>
2022-11-10 20:18:54 -0300

    registro usuario

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -9,6 +9,22 @@ async function ShowFeatured(req, res) {
   res.json(featured);
 }
 
+async function storeUser(req, res) {
+  console.log("hola");
+  const newUser = await new User({
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    email: req.body.email,
+    password: req.body.password,
+    adress: req.body.adress,
+    phoneNumber: req.body.phoneNumber,
+  });
+
+  await newUser.save();
+  res.json("algo paso");
+}
+
 module.exports = {
   ShowFeatured,
+  storeUser,
 };
