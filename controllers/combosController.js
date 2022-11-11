85ddc8c57a0cdd627c868859db29d8b24bde8bcf
fgomensoro <102269369+fgomensoro@users.noreply.github.com>
2022-11-11 09:28:41 -0300

    feat: combo route, controller

---
+++ controllers/combosController.js
@@ -0,0 +1,11 @@
+const Combo = require("../models/Combo");
+
+
+async function ShowCombos(req, res) {
+  const combos = await Combo.find();
+  res.json(combos);
+}
+
+module.exports = {
+  ShowCombos,
+};
