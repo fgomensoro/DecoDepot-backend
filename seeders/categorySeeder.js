b1c6baf092a322ee8dd6c8a0e2c9b6fe6908b592
fgomensoro <102269369+fgomensoro@users.noreply.github.com>
2022-11-23 02:24:36 -0300

    fix: plural to categories

--- seeders/categorySeeder.js
+++ seeders/categorySeeder.js
@@ -3,7 +3,7 @@ const Category = require("../models/Category");
 const { mongoose } = require("mongoose");
 
 module.exports = async () => {
-  const categories = ["Table", "Chair", "Bed", "Sofa", "Décor"];
+  const categories = ["Tables", "Chairs", "Beds", "Sofas", "Décor"];
   for (let i = 0; i < categories.length; i++) {
     const category = new Category({
       name: categories[i],
