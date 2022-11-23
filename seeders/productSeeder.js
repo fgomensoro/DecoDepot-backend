36d3b92bd53f02c4f85e7f2652ba3c21fd2b6fcb
Joel Dominguez <joeru.desu@gmail.com>
2022-11-23 09:31:33 -0300

    fix productSeeders, added beds and tables

--- seeders/productSeeder.js
+++ seeders/productSeeder.js
@@ -141,6 +141,189 @@ module.exports = async () => {
     slug: "irving_wood_table_lamp",
   });
 
+  // ================ Tables ============================================
+
+  await Product.create({
+    name: "T brace Console Table",
+    description: `Inspired by the American Industrial Age, our collection by Julie Lawrence evokes its iconic infrastructure. Solid oak timbers are supported on tapered stanchions of hand-cast metal, the mix of materials and textures imparting a rugged, organic aesthetic. Carefully calibrated proportions lend refinement.`,
+    images: [
+      "T_brace_Console_Table_1.jpg",
+      "T_brace_Console_Table_2.jpg",
+      "T_brace_Console_Table_3.jpg",
+    ],
+    price: 3200,
+    stock: 6,
+    featured: false,
+    category: categories[0],
+    slug: "T_brace_Console_Table",
+  });
+
+  await Product.create({
+    name: "Reclaimed Oak Parsons Console Table",
+    description: `Reclaimed oak timbers from decades-old buildings, with distressed white oak planks for consistency and stability, lend richness to this simple, linear collection from artisan Theo Eichholtz. Each piece is handcrafted of wood that retains its natural character, bearing the occasional knot, nick or crack where no two tables are exactly the same.`,
+    images: [
+      "Reclaimed_Oak_Parsons_Console_Table_1.jpg",
+      "Reclaimed_Oak_Parsons_Console_Table_2.jpg",
+      "Reclaimed_Oak_Parsons_Console_Table_3.jpg",
+    ],
+    price: 2500,
+    stock: 10,
+    featured: true,
+    category: categories[0],
+    slug: "Reclaimed_Oak_Parsons_Console_Table",
+  });
+
+  await Product.create({
+    name: "Davos Oak Console Table",
+    description: `Inspired by the 1970s use of bold lines and natural forms, the Davos collection by Australians Nicholas and Harrison Condos is built on a heroic scale. The spare silhouettes, rendered in thick veneers of American white oak, reveal an intriguing juxtaposition of crisp angles and rounded corners. The oak's texture is enhanced by a high-heat finishing process that creates distinctive cracks and purposeful imperfections, a celebration of the wood's character and unique grain patterning.`,
+    images: [
+      "Davos_Oak_Console_Table_1.jpg",
+      "Davos_Oak_Console_Table_2.jpg",
+      "Davos_Oak_Console_Table_3.jpg",
+    ],
+    price: 2700,
+    stock: 3,
+    featured: false,
+    category: categories[0],
+    slug: "Davos_Oak_Console_Table",
+  });
+
+  await Product.create({
+    name: "Bardot Burl Rectangular Dining Table",
+    description: `Understated and tailored in its restraint, our collection honors the minimalist geometric forms of 1970s postmodernism and the era's embrace of exotic materials. The clean, gently curved silhouettes are wrapped in burlwood veneers, which highlight the intricate, swirling grain patterns and mottled shading. Expertly cut and pieced by hand, the burl is sanded and buffed to an elegant sheen.`,
+    images: [
+      "Bardot_Burl_Rectangular_Dining_Table_1.jpg",
+      "Bardot_Burl_Rectangular_Dining_Table_2.jpg",
+      "Bardot_Burl_Rectangular_Dining_Table_3.jpg",
+    ],
+    price: 3800,
+    stock: 1,
+    featured: false,
+    category: categories[0],
+    slug: "Bardot_Burl_Rectangular_Dining_Table",
+  });
+
+  await Product.create({
+    name: "Oslo Cylinder Rectangular Dining Table",
+    description: `Inspired by the simplicity of early Swedish Modernism, the boldly scaled Oslo collection by Australians Nicholas and Harrison Condos is a contemporary celebration of the spare silhouettes and gentle contours of the natural world. Defined by a sculptural grouping of cylindrical supports, the tables are meticulously handcrafted of wire-brushed American white oak. A high-heat finishing process creates distinctive cracking, further enhancing the wood's rich texture and unique grain patterning — an elegant juxtaposition of rustic and relaxed, minimalist and refined.`,
+    images: [
+      "Oslo_Cylinder_Rectangular_Dining_Table_1.jpg",
+      "Oslo_Cylinder_Rectangular_Dining_Table_2.jpg",
+      "Oslo_Cylinder_Rectangular_Dining_Table_3.jpg",
+    ],
+    price: 4200,
+    stock: 3,
+    featured: true,
+    category: categories[0],
+    slug: "Oslo_Cylinder_Rectangular_Dining_Table",
+  });
+
+  await Product.create({
+    name: "T brace Rectangular Extension Dining Table",
+    description: `Inspired by the American Industrial Age, our collection by Julie Lawrence evokes its iconic infrastructure. Solid oak timbers are supported on tapered stanchions of hand-cast metal, the mix of materials and textures imparting a rugged, organic aesthetic. Carefully calibrated proportions lend refinement.`,
+    images: [
+      "T_brace_Rectangular_Extension_Dining_Table_1.jpg",
+      "T_brace_Rectangular_Extension_Dining_Table_2.jpg",
+      "T_brace_Rectangular_Extension_Dining_Table_3.jpg",
+    ],
+    price: 6200,
+    stock: 3,
+    featured: true,
+    category: categories[0],
+    slug: "T_brace_Rectangular_Extension_Dining_Table",
+  });
+
+  // ================ Beds ==============================================
+  await Product.create({
+    name: "Cortona Fabric Panel Platform Bed",
+    description: `Designed by Ann Marie Vering of Los Angeles, the Cortona collection evokes the architectural language of Italian postmodernism. The bed's angular silhouette is a study in geometric forms, harmoniously coexisting, with a crisp, block-like headboard rests atop a recessed platform base, lending it the appearance of floating.`,
+    images: [
+      "Cortona_Fabric_Panel_Platform_Bed_1.jpg",
+      "Cortona_Fabric_Panel_Platform_Bed_2.jpg",
+      "Cortona_Fabric_Panel_Platform_Bed_3.jpg",
+    ],
+    price: 8800,
+    stock: 4,
+    featured: true,
+    category: categories[2],
+    slug: "Cortona_Fabric_Panel_Platform_Bed",
+  });
+
+  await Product.create({
+    name: "Vittorio Fabric Panel Platform Bed With Footboard",
+    description: `Designed by Ann Marie Vering of Los Angeles, the Cortona collection evokes the architectural language of Italian postmodernism. The bed's angular silhouette is a study in geometric forms, harmoniously coexisting, with a crisp, block-like headboard rests atop a recessed platform base, lending it the appearance of floating.`,
+    images: [
+      "Vittorio_Fabric_Panel_Platform_Bed_With_Footboard_1.jpg",
+      "Vittorio_Fabric_Panel_Platform_Bed_With_Footboard_2.jpg",
+      "Vittorio_Fabric_Panel_Platform_Bed_With_Footboard_3.jpg",
+    ],
+    price: 7700,
+    stock: 1,
+    featured: false,
+    category: categories[2],
+    slug: "Vittorio_Fabric_Panel_Platform_Bed_With_Footboard",
+  });
+
+  await Product.create({
+    name: "Modena Fabric Panel Patform Bed",
+    description: `Distinguished by crisp lines and smooth contours, our sculptural platform beds are fully customizable. Create your perfect bed, down to the last detail – from headboard height and style to upholstery fabrics or leathers. Our nontufted panel bed's unadorned, hand-upholstered headboard lends elegance and luxurious comfort.`,
+    images: [
+      "Modena_Fabric_Panel_Patform_Bed_1.jpg",
+      "Modena_Fabric_Panel_Patform_Bed_2.jpg",
+      "Modena_Fabric_Panel_Patform_Bed_3.jpg",
+    ],
+    price: 3900,
+    stock: 5,
+    featured: true,
+    category: categories[2],
+    slug: "Modena_Fabric_Panel_Patform_Bed",
+  });
+
+  await Product.create({
+    name: "Cayden Campaign Fabric Panel Bed",
+    description: `European campaign furnishings of the 19th century inspired our collection by the Van Thiels, echoed in the simple strength of the shapes and the rustic grain of the solid oak. Subtle details including tapered legs and rich upholstery lend refinement to the clean, forthright silhouettes.`,
+    images: [
+      "Cayden_Campaign_Fabric_Panel_Bed_1.jpg",
+      "Cayden_Campaign_Fabric_Panel_Bed_2.jpg",
+      "Cayden_Campaign_Fabric_Panel_Bed_3.jpg",
+    ],
+    price: 4290,
+    stock: 7,
+    featured: true,
+    category: categories[2],
+    slug: "Cayden_Campaign_Fabric_Panel_Bed",
+  });
+
+  await Product.create({
+    name: "Modena Fabric Framed Panel Platform Bed",
+    description: `Distinguished by crisp lines and smooth contours, our fully customizable platform bed is set atop a recessed base that lends the appearance of floating. Create your perfect bed, down to the last detail – from headboard height and style to upholstery fabrics or leathers. The sculptural form of our nontufted panel bed is anchored by a hand-upholstered headboard framed in richly textured oak.`,
+    images: [
+      "Modena_Fabric_Framed_Panel_Platform_Bed_1.jpg",
+      "Modena_Fabric_Framed_Panel_Platform_Bed_2.jpg",
+      "Modena_Fabric_Framed_Panel_Platform_Bed_3.jpg",
+    ],
+    price: 4190,
+    stock: 3,
+    featured: false,
+    category: categories[2],
+    slug: "Modena_Fabric_Framed_Panel_Platform_Bed",
+  });
+
+  await Product.create({
+    name: "Cayden Campaign Fabric Panel Bed With Footboard",
+    description: `European campaign furnishings of the 19th century inspired our collection by the Van Thiels, echoed in the simple strength of the shapes and the rustic grain of the solid oak. Subtle details including tapered legs and rich upholstery lend refinement to the clean, forthright silhouettes.`,
+    images: [
+      "Cayden_Campaign_Fabric_Panel_Bed_With_Footboar_1.jpg",
+      "Cayden_Campaign_Fabric_Panel_Bed_With_Footboar_2.jpg",
+      "Cayden_Campaign_Fabric_Panel_Bed_With_Footboar_3.jpg",
+    ],
+    price: 5190,
+    stock: 5,
+    featured: true,
+    category: categories[2],
+    slug: "Cayden_Campaign_Fabric_Panel_Bed_With_Footboard",
+  });
+
   // ================ Chairs ============================================
 
   await Product.create({
